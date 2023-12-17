45cf75bcdf43f08bd7963f96af06c5dac50b5a3b
rasel-mahmud-dev <rasel.mahmud.dev@gmail.com>
2023-12-17 14:54:44 +0600

    Added mobile category slider

--- example/src/pages/Products.jsx
+++ example/src/pages/Products.jsx
@@ -3,6 +3,8 @@ import {useDispatch, useSelector} from "rsl-redux";
 import Product from "../component/Product.jsx";
 import {fetchProducts} from "../store/actions/productAction.js";
 import HeroBanner from "../component/HeroBanner.jsx";
+import {Link} from "react-router-dom";
+import getAssetPath from "../utils/getAssetPath.js";
 
 const Products = () => {
     const {products, categories} = useSelector((state) => state.productState)
@@ -14,9 +16,24 @@ const Products = () => {
         dispatch(fetchProducts())
     }, [])
 
+
     return (
         <div>
-            <HeroBanner categories={categories}  />
+            <div className="home-category-list">
+                <div
+                    className="flex items-center justify-between max-w-8xl mx-auto  gap-x-2 md:gap-x-4 scroll-x-transparent overflow-x-auto md:overflow-visible ">
+                    {categories.map((cat) => (
+                        <div key={cat._id} className="home-category-list-item">
+                            <div
+                                className="flex home-category-list-item-content flex-col items-center  border md:border-none rounded-full md:bg-transparent ">
+                                <img alt={cat.name} className="category-list-item-img" src={getAssetPath(cat.image)}/>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+
+            <HeroBanner categories={categories} />
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-2 md:gap-3 lg:gap-6 mt-4">
                 {products.map(product => (
                     <Product key={product.id} {...product} />
